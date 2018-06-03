ce90856283d03e92fd6c498b7addfcf890e98106
sxyzc <syxzc21@zoho.com.cn>
2018-06-03 20:57:43 +0800

    发布博客界面基本完成，包括创建标签

--- src/components/page/EditBlog.vue
+++ src/components/page/EditBlog.vue
@@ -4,10 +4,10 @@
 <template>
     <div>
         <div style="margin-top: 15px;">
-            <el-input placeholder="请输入标题" v-model="input">
+            <el-input placeholder="请输入标题" v-model="blogTitle">
                 <el-button slot="append" @click="prePublish" >发布</el-button>
                 <el-button slot="append" @click="testLabel" >添加标签</el-button>
-                <el-button slot="append" @click="updateTags" >输出标签</el-button>
+                <el-button slot="append" @click="getTags" >输出标签</el-button>
                 <el-button slot="append" @click="testLogin" >登录</el-button>
             </el-input>
         </div>
@@ -26,13 +26,13 @@
         <br>
         <div>
             <el-row><el-col :span="4">
-            <span style="float:left;">标签: </span>
+            <span style="float:left;">新建标签: </span>
             </el-col>
             <el-col :span="20">
         <el-tag
         size="mini" 
         :key="tag"
-        v-for="tag in dynamicTags"
+        v-for="tag in newTags"
         closable
         :disable-transitions="false"
         @close="handleCloseTag(tag)">
@@ -59,19 +59,19 @@
         <span style="float:left;">已有标签: </span></el-col>    
         
             <el-col :span="20">
-            <el-checkbox-group v-model="checkedLabels" @change="handleCheckedLabelsChange">
-                <el-checkbox v-for="my_label in my_labels" :label="my_label" :key="my_label">{{my_label}}</el-checkbox>
+            <el-checkbox-group v-model="checkedTags" @change="handleCheckedTagsChange">
+                <el-checkbox v-for="my_label in tags" :label="my_label" :key="my_label">{{my_label}}</el-checkbox>
             </el-checkbox-group></el-col>
             </el-row>
         </div>
         <span slot="footer" class="dialog-footer">
             <el-button @click="dialogVisible = false">取 消</el-button>
-            <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+            <el-button type="primary" @click="publish">确 定</el-button>
         </span>
         </el-dialog>
         <br>
     <!-- <mavon-editor ref=md @imgAdd="$imgAdd" @imgDel="$imgDel"></mavon-editor> -->
-    <mavon-editor ref=md @save="$save"></mavon-editor>
+    <mavon-editor v-model="blogContent" ref=md @save="$save"></mavon-editor>
     </div>
 </template>
 
@@ -103,56 +103,115 @@
   export default {
     data() {
       return {
-        input: '',
         dialogVisible: false,
-        checkedLabels: ['类一', '类二'],
-        my_labels: ['类一', '类二', '类三', '类四'],
+        checkedTags: [],
+        tags: [],
         
-        dynamicTags: ['标签一', '标签二', '标签三'],
+        newTags: [],
+        newTagIds:[],
+        tagIds:[],
         inputVisible: false,
-        inputValue: ''
+        inputValue: '',
+        blogContent:'',
+        blogTitle:''
         
       };
     },
     methods: {
         onSubmit() {
             console.log('submit!');
-            //console.log(mavonEditor.markdownIt)
-            // this.$alert('<strong>这是 <i>HTML</i> 片段</strong><el-button type="primary">主要按钮</el-button>', 'HTML 片段', {
-            //     dangerouslyUseHTMLString: true
-            //     });
             console.log(mavonEditor.markdownIt.render()+'@@');
         },
         prePublish(){
             this.dialogVisible = true;
         },
-        updateTags(){
-            console.log("output tags!");
+        publish(){
+            console.log("publish!");
+            this.dialogVisible = false;
+            console.log(this.blogTitle);
+            console.log(this.checkedTags);
+            console.log(this.newTags);
+            console.log(this.blogContent);
+
+            if(this.newTags.length>0){
+                this.$axios({
+                    url: 'http://39.108.73.165:8080/Blog/createTag',
+                    method: 'post',
+                    data: {
+                        tagContent: this.newTags,
+                    }
+                })
+                .then((response) => {
+                    console.log("create tag!");
+                    console.log(response.data)
+                    this.newTagIds=response.data;
+                    this.createBlog();
+                })
+                .catch((error) => {
+                    this.$notify({
+                        title: '失败',
+                        message: '创建标签失败: ' + '请重试!',
+                        type: 'error'
+                    });
+                    console.log("【Error】:", error);
+                });
+            }
+            else 
+            this.createBlog();
+        },
+        createBlog(){
+            console.log("create blog!");
+            var allTags=[];
+            for(var i=0;i<this.newTagIds.length;i++)
+                allTags.push(this.newTagIds[i]);
+            for(var i=0;i<this.checkedTags.length;i++){
+                for(var j=0;j<this.tags.length;j++){
+                    if(this.tags[j]==this.checkedTags[i])allTags.push(this.tagIds[j]);
+                }
+            }
+            console.log("ok!");
+            this.$axios({
+                url: 'http://39.108.73.165:8080/Blog/createBlog',
+                method: 'post',
+                data: {
+                    blogContent: this.blogContent,
+                    blogTitle: this.blogTitle,
+                    tags: allTags
+                }
+            })
+            .then((response) => {
+                if (response.status === 200) {
+                    this.$notify({
+                        title: '成功',
+                        message: '发布博客成功!',
+                        type: 'success'
+                    });
+                }
+            })
+            .catch((error) => {
+                this.$notify({
+                    title: '失败',
+                    message: '发布失败: ' + '请重试!',
+                    type: 'error'
+                });
+                console.log("【Error】:", error);
+            });
+        },
+        getTags(){
+            console.log("get tags!");
             this.$axios({
                 url: 'http://39.108.73.165:8080/Blog/tagById',
                 method: 'get',
-                xhrFields: {
-                    withCredentials: true
-                },
-                data: {
-                }
             })
             .then((response) => {
                 console.log(response);
                 if (response.status === 200) {
-                    this.dynamicTags=[];
+                    this.tags=[];
+                    this.tagIds=[];
                     for(var i=0;i<response.data.length;i++){
-                        this.dynamicTags.push(response.data[i]['tagContent']);
+                        this.tags.push(response.data[i]['tagContent']);
+                        this.tagIds.push(response.data[i]['tagId']);
                     }
-                    // this.$notify({
-                    //     title: '成功',
-                    //     message: '读取标签成功!',
-                    //     type: 'success'
-                    // });
-                } else {
-                    console.log("no 200!");
-                    console.log(response.status);
-
                 }
             })
             .catch((error) => {
@@ -166,19 +225,12 @@
         },
         testLabel() {
             console.log('create Label!');
-            //console.log(mavonEditor.markdownIt)
-            // this.$alert('<strong>这是 <i>HTML</i> 片段</strong><el-button type="primary">主要按钮</el-button>', 'HTML 片段', {
-            //     dangerouslyUseHTMLString: true
-            //     });
+
             this.$axios({
                 url: 'http://39.108.73.165:8080/Blog/createTag',
                 method: 'post',
-                //baseURL: this.hostUrl,
-                xhrFields: {
-                    withCredentials: true
-                },
                 data: {
-                    tagContent: "cpp",
+                    tagContent: "php",
                 }
             })
             .then((response) => {
@@ -191,9 +243,7 @@
                         type: 'success'
                     });
                 } else {
-                    console.log("no 200!");
                     console.log(response.status);
-
                 }
             })
             .catch((error) => {
@@ -207,20 +257,11 @@
         },
         testLogin() {
             console.log('login!');
-            //console.log(mavonEditor.markdownIt)
-            // this.$alert('<strong>这是 <i>HTML</i> 片段</strong><el-button type="primary">主要按钮</el-button>', 'HTML 片段', {
-            //     dangerouslyUseHTMLString: true
-            //     });
+
+
             this.$axios({
                 url: 'http://39.108.73.165:8080/Blog/login',
                 method: 'post',
-    //             headers: {
-    //     "Content-Type":"application/json;charset=utf-8"
-    //    },
-                //baseURL: this.hostUrl,
-                // xhrFields: {
-                //     withCredentials: true
-                // },
                 data: {
                     email: "123@qq.com",
                     password: "123",
@@ -252,7 +293,7 @@
             });
         },
         $save(t_value, t_render){
-            console.log('111111');
+            console.log('save markdown!');
             console.log(t_value);
         },
         handleClose(done) {
@@ -262,13 +303,13 @@
             })
             .catch(_ => {});
         },
-        handleCheckedLabelsChange(value) {
+        handleCheckedTagsChange(value) {
             let checkedCount = value.length;
-            this.checkAll = checkedCount === this.my_labels.length;
-            this.isIndeterminate = checkedCount > 0 && checkedCount < this.my_labels.length;
+            this.checkAll = checkedCount === this.tags.length;
+            this.isIndeterminate = checkedCount > 0 && checkedCount < this.tags.length;
         },
         handleCloseTag(tag) {
-            this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
+            this.newTags.splice(this.newTags.indexOf(tag), 1);
         },
         showInput() {
             this.inputVisible = true;
@@ -279,30 +320,16 @@
         handleInputConfirm() {
             let inputValue = this.inputValue;
             if (inputValue) {
-            this.dynamicTags.push(inputValue);
+            this.newTags.push(inputValue);
             }
             this.inputVisible = false;
             this.inputValue = '';
         }
-    //   $imgAdd(pos, $file){
-    //         // 第一步.将图片上传到服务器.
-    //        var formdata = new FormData();
-    //        formdata.append('image', $file);
-    //        axios({
-    //            url: 'server url',
-    //            method: 'post',
-    //            data: formdata,
-    //            headers: { 'Content-Type': 'multipart/form-data' },
-    //        }).then((url) => {
-    //            // 第二步.将返回的url替换到文本原位置![...](./0) -> ![...](url)
-    //            /**
-    //            * $vm 指为mavonEditor实例，可以通过如下两种方式获取
-    //            * 1. 通过引入对象获取: `import {mavonEditor} from ...` 等方式引入后，`$vm`为`mavonEditor`
-    //            * 2. 通过$refs获取: html声明ref : `<mavon-editor ref=md ></mavon-editor>，`$vm`为 `this.$refs.md`
-    //            */
-    //            $vm.$img2Url(pos, url);
-    //        })
-    //     }
+       
+    },
+    mounted(){
+            console.log("mounted!");
+            this.getTags();
     }
   };
 </script>
